e29b26bdf06ccbb4966b6eaa3163bdd630b90879
Baekdong Cha <bcha@scc4.bu.edu>
2022-04-22 13:24:45 -0400

    진행-코드통합작업_training완료

---
+++ modeltraining/ss_momentModel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#$ -l h_rt=48:00:00
+#$ -N moment
+#$ -o result_qsub/moment
+#$ -j y    
+#$ -m ea
+#$ -l gpus=1
+#$ -pe omp 8
+#$ -l mem_per_core=8G
+
+
+module load miniconda/4.9.2
+conda activate sccIMU
+
+# NSLOTS will be set to the # of requested cores
+
+export TF_NUM_INTEROP_THREADS=$(( $NSLOTS - 1 ))
+export TF_NUM_INTRAOP_THREADS=1
+python batch_5fold_momentModel.py
