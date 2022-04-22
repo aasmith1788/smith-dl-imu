9b112ef9db19b1fb7b8add0b688c430d4f089014
Baekdong Cha <bcha@scc1.bu.edu>
2022-04-22 10:43:24 -0400

    완료--angleModel에 nRMSE 적용

--- batch_5fold_angleModel.py
+++ batch_5fold_angleModel.py
@@ -96,6 +96,55 @@ def Z_Axis_RMSE(y_true, y_pred):
     print(f"\nUsing {numFold} fold scaler")
     return Z_Axis_RMSE
 
+def rescaled_RMSE_pct(y_true, y_pred):
+    y_true = tf.transpose(tf.reshape(tf.squeeze(y_true), [3,-1]))
+    y_pred = tf.transpose(tf.reshape(tf.squeeze(y_pred), [3,-1]))
+    y_true = (y_true - K.constant(load_scaler4Y_moBHWT.min_)) / K.constant(load_scaler4Y_moBHWT.scale_)
+    y_pred = (y_pred - K.constant(load_scaler4Y_moBHWT.min_)) / K.constant(load_scaler4Y_moBHWT.scale_)
+    # default is RMSE, squaredbool, default=True If True returns MSE value, if False returns RMSE value.
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
+    rescaled_RMSE_pct = 100 * K.sqrt(K.mean(K.square(y_pred - y_true))) / (K.max(y_true) - K.min(y_true))
+    print(f"\nUsing {numFold} fold scaler")
+    return rescaled_RMSE_pct
+
+def X_Axis_RMSE_pct(y_true, y_pred):
+    NumAxis = 0
+    y_true = tf.transpose(tf.reshape(tf.squeeze(y_true), [3,-1]))[:,NumAxis]
+    y_pred = tf.transpose(tf.reshape(tf.squeeze(y_pred), [3,-1]))[:,NumAxis]
+    print(y_true.shape)
+    y_true = (y_true - K.constant(load_scaler4Y_moBHWT.min_[NumAxis])) / K.constant(load_scaler4Y_moBHWT.scale_[NumAxis])
+    y_pred = (y_pred - K.constant(load_scaler4Y_moBHWT.min_[NumAxis])) / K.constant(load_scaler4Y_moBHWT.scale_[NumAxis])
+    # default is RMSE, squaredbool, default=True If True returns MSE value, if False returns RMSE value.
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
+    X_Axis_RMSE_pct = 100 * K.sqrt(K.mean(K.square(y_pred - y_true))) / (K.max(y_true) - K.min(y_true))
+    print(f"\nUsing {numFold} fold scaler")
+    return X_Axis_RMSE_pct
+    
+def Y_Axis_RMSE_pct(y_true, y_pred):
+    NumAxis = 1
+    y_true = tf.transpose(tf.reshape(tf.squeeze(y_true), [3,-1]))[:,NumAxis]
+    y_pred = tf.transpose(tf.reshape(tf.squeeze(y_pred), [3,-1]))[:,NumAxis]
+    print(y_true.shape)
+    y_true = (y_true - K.constant(load_scaler4Y_moBHWT.min_[NumAxis])) / K.constant(load_scaler4Y_moBHWT.scale_[NumAxis])
+    y_pred = (y_pred - K.constant(load_scaler4Y_moBHWT.min_[NumAxis])) / K.constant(load_scaler4Y_moBHWT.scale_[NumAxis])
+    # default is RMSE, squaredbool, default=True If True returns MSE value, if False returns RMSE value.
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
+    Y_Axis_RMSE_pct = 100 * K.sqrt(K.mean(K.square(y_pred - y_true))) / (K.max(y_true) - K.min(y_true))
+    print(f"\nUsing {numFold} fold scaler")
+    return Y_Axis_RMSE_pct
+
+def Z_Axis_RMSE_pct(y_true, y_pred):
+    NumAxis = 2
+    y_true = tf.transpose(tf.reshape(tf.squeeze(y_true), [3,-1]))[:,NumAxis]
+    y_pred = tf.transpose(tf.reshape(tf.squeeze(y_pred), [3,-1]))[:,NumAxis]
+    print(y_true.shape)
+    y_true = (y_true - K.constant(load_scaler4Y_moBHWT.min_[NumAxis])) / K.constant(load_scaler4Y_moBHWT.scale_[NumAxis])
+    y_pred = (y_pred - K.constant(load_scaler4Y_moBHWT.min_[NumAxis])) / K.constant(load_scaler4Y_moBHWT.scale_[NumAxis])
+    # default is RMSE, squaredbool, default=True If True returns MSE value, if False returns RMSE value.
+    # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.mean_squared_error.html
+    Z_Axis_RMSE_pct = 100 * K.sqrt(K.mean(K.square(y_pred - y_true))) / (K.max(y_true) - K.min(y_true))
+    print(f"\nUsing {numFold} fold scaler")
+    return Z_Axis_RMSE_pct
 
 # 데이터 셋 준비
 # 데이터 셋 준비
@@ -133,7 +182,7 @@ for numFold in range(0,5): # 5-fold crossvalidation
     model = create_model()
     model.compile(optimizer=myoptim,
               loss='mean_absolute_error',
-              metrics=[X_Axis_RMSE, Y_Axis_RMSE, Z_Axis_RMSE])
+              metrics=[X_Axis_RMSE_pct, Y_Axis_RMSE_pct, Z_Axis_RMSE_pct])
     # 차원 축소
     X_train = np.squeeze(load_train["final_X_train"], axis=2)
     Y_angle_train = np.squeeze(load_train["final_Y_angle_train"], axis=2)
