e4e47d89878bf4453dab54b45ff3b0b2f1450da6
SCC_CBD <orientpine@gmail.com>
2022-05-11 11:07:52 -0400

    feat: Dense 모델에서 AE 학습데이터로 학습할 수 있게 작업함

--- training/grid_torch_momentModel.py
+++ training/grid_torch_momentModel.py
@@ -81,6 +81,10 @@ class Dataset(torch.utils.data.Dataset):
       self.sess = sess # train or test
       self.load_Data_X = torch.from_numpy(np.load(join(dataSetDir,f"{str(numFold)}_fold_final_{sess}.npz"))[f'final_X_{self.sess}'])
       self.load_Data_Y = torch.from_numpy(np.load(join(dataSetDir,f"{str(numFold)}_fold_final_{sess}.npz"))[f'final_Y_{self.dataType}_{self.sess}'])
+      # AE데이터로 Dense 학습 시킬 때
+      self.load_Data_X = np.reshape(self.load_Data_X, newshape=(-1,self.load_Data_X.shape[1]*self.load_Data_X.shape[2],1), order='F')
+      self.load_Data_Y = np.reshape(self.load_Data_Y, newshape=(-1,self.load_Data_Y.shape[1]*self.load_Data_Y.shape[2],1), order='F')
+
       self.load_Data_X = torch.squeeze(self.load_Data_X.type(torch.FloatTensor))
       self.load_Data_Y = torch.squeeze(self.load_Data_Y.type(torch.FloatTensor))
   def __len__(self):
