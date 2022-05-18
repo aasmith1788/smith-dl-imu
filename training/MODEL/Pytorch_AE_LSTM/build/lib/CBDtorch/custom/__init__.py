300f162e72297e10ba7264203051e426a4970414
SCC_CBD <orientpine@gmail.com>
2022-05-18 10:17:42 -0400

    refactor: 기존 코드를 라이브러리로 만들어보기

---
+++ training/MODEL/Pytorch_AE_LSTM/build/lib/CBDtorch/custom/__init__.py
@@ -0,0 +1,3 @@
+from .rmseloss import RMSELoss
+from .dataset import Dataset
+from .minmaxscalersensor import MinMaxScalerSensor
---
+++ training/MODEL/Pytorch_AE_LSTM/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup, find_packages  
+setup(name = 'CBDtorch', packages = find_packages(), version = "0.1.0")
