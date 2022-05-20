96417d69c437ded20ed540c472360d97a6782acc
BUcom_CBD <orientpine@gmail.com>
2022-05-20 10:39:25 -0400

    docs: 커스텀 라이브러리 설정 파일 구동방법 추가

--- training/MODEL/Pytorch_AE_LSTM/setup.py
+++ training/MODEL/Pytorch_AE_LSTM/setup.py
@@ -1,2 +1,3 @@
-from setuptools import setup, find_packages  
-setup(name = 'CBDtorch', packages = find_packages(), version = "0.1.0")
+# python setup.py install
+from setuptools import setup, find_packages
+setup(name='CBDtorch', packages=find_packages(), version="0.1.0")
