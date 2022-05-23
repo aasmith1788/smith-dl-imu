51b603e5c1e03059a6ac68e5e0801b8714a4f410
BUcom_CBD <orientpine@gmail.com>
2022-05-23 15:13:15 -0400

    feat: regressor 용 qsub개발 완료

---
+++ training/MODEL/Pytorch_AE_LSTM/qsub_torch_regressor_angle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#$ -l h_rt=10:00:00
+#$ -N regAng
+#$ -o ../../result_qsub/regAng/try_1st
+#$ -j y    
+#$ -m ea
+#$ -l gpus=1
+#$ -l gpu_c=6.0
+#$ -pe omp 8
+# 메모리에 관해서는 아래 주소를 참고
+# https://www.bu.edu/tech/support/research/system-usage/running-jobs/batch-script-examples/#MEMORY
+
+module load miniconda/4.9.2
+conda activate imu
+python torch_regression_angle.py
