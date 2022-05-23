877990121b079fc5c598910b6d8e9569353b3fbd
BUcom_CBD <orientpine@gmail.com>
2022-05-23 09:26:01 -0400

    feat: 커스텀 py env 추가 방법 변경

--- training/MODEL/Pytorch_AE_LSTM/setup.py
+++ training/MODEL/Pytorch_AE_LSTM/setup.py
@@ -1,3 +1,10 @@
-# python setup.py install
+# python setup.py install 는 더이상 사용하지 않음
+# python setup.py install -> pip install -e .
 from setuptools import setup, find_packages
 setup(name='CBDtorch', packages=find_packages(), version="0.1.0")
+
+
+# 참고
+# pip install -e .
+# from setuptools import setup
+# setup(name='larva_duo',version='0.0.1',install_requires=['gym','pybullet'])
