2d589a661b8538c885f5620b1b2530fe18e2beb9
SCC_CBD <orientpine@gmail.com>
2022-05-25 11:49:45 -0400

    fix: 코드를 잘못 짜서 똑같은 코드만 3번돌려지는 것 수정함

--- training/MODEL/Pytorch_AE_LSTM/torch_VAE_LSTM.py
+++ training/MODEL/Pytorch_AE_LSTM/torch_VAE_LSTM.py
@@ -52,88 +52,89 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # grid search 시, 현재의 count
 count = 0
 for opt1 in range(0,len(list_learningRate)):
-    for opt2 in range(0,len(list_batch_size)):
+    for opt2 in range(0,len(list_lossFunction)):
         for opt3 in range(0,len(list_embedding_dim)):
-            learningRate = list_learningRate[opt1]
-            batch_size = list_batch_size[opt2]
-            lossFunction = list_lossFunction[0]
-            embedding_dim = list_embedding_dim[0]
-            print(f"count:{count} | 현재 설정 Type:{dataType}, lr:{learningRate}, BS:{batch_size}, LF:{lossFunction}, emb_dim:{embedding_dim}, \
-                \nmodelV:{modelVersion}, DataSet:{nameDataset}")
-            count = count + 1 
-            # 조건 별로 시작 시간 기록하기
-            time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S%f")[:-2]
-            for numFold  in range(totalFold):
-                print(f'now fold: {numFold}')
-                # 매 fold마다 새로운 모델
-                my_model = RecurrentVariationalAutoencoder(seq_len, num_features, embedding_dim, device)
-                my_model.to(device)
-                
-                # loss function and optimizer define
-                optimizer = torch.optim.NAdam(my_model.parameters(),lr=learningRate)
+            for opt4 in range(0,len(list_batch_size)):
+                learningRate = list_learningRate[opt1]
+                lossFunction = list_lossFunction[opt2]
+                embedding_dim = list_embedding_dim[opt3]
+                batch_size = list_batch_size[opt4]                
+                print(f"count:{count} | 현재 설정 Type:{dataType}, lr:{learningRate}, BS:{batch_size}, LF:{lossFunction}, emb_dim:{embedding_dim}, \
+                    \nmodelV:{modelVersion}, DataSet:{nameDataset}")
+                count = count + 1 
+                # 조건 별로 시작 시간 기록하기
+                time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S%f")[:-2]
+                for numFold  in range(totalFold):
+                    print(f'now fold: {numFold}')
+                    # 매 fold마다 새로운 모델
+                    my_model = RecurrentVariationalAutoencoder(seq_len, num_features, embedding_dim, device)
+                    my_model.to(device)
+                    
+                    # loss function and optimizer define
+                    optimizer = torch.optim.NAdam(my_model.parameters(),lr=learningRate)
 
-                angle_train = Dataset4autoencoder(dataSetDir, dataType, 'train',numFold)
-                angle_test  = Dataset4autoencoder(dataSetDir, dataType, 'test', numFold)
-                train_loader = DataLoader(angle_train, batch_size=batch_size, shuffle=True)
-                test_loader = DataLoader(angle_test, batch_size=batch_size, shuffle=True)
+                    angle_train = Dataset4autoencoder(dataSetDir, dataType, 'train',numFold)
+                    angle_test  = Dataset4autoencoder(dataSetDir, dataType, 'test', numFold)
+                    train_loader = DataLoader(angle_train, batch_size=batch_size, shuffle=True)
+                    test_loader = DataLoader(angle_test, batch_size=batch_size, shuffle=True)
 
-                # 시각화를 위한 tensorboard 초기화
-                writer_train = SummaryWriter(join(logDir,f'{exp_name}/{modelVersion}/{nameDataset}/{dataType}/LR_{learningRate}_BS_{batch_size}_embdim_{embedding_dim}/train/{numFold}_fold'))
-                writer_test =  SummaryWriter(join(logDir,f'{exp_name}/{modelVersion}/{nameDataset}/{dataType}/LR_{learningRate}_BS_{batch_size}_embdim_{embedding_dim}/test/{numFold}_fold'))
+                    # 시각화를 위한 tensorboard 초기화
+                    writer_train = SummaryWriter(join(logDir,f'{exp_name}/{modelVersion}/{nameDataset}/{dataType}/LR_{learningRate}_BS_{batch_size}_embdim_{embedding_dim}/train/{numFold}_fold'))
+                    writer_test =  SummaryWriter(join(logDir,f'{exp_name}/{modelVersion}/{nameDataset}/{dataType}/LR_{learningRate}_BS_{batch_size}_embdim_{embedding_dim}/test/{numFold}_fold'))
 
-                for epoch in range(epochs):
-                    # train session
-                    my_model.train()
-                    train_loss = 0
-                    for batch_idx, (data) in enumerate(tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs}")):
-                        # print(f'datainput shape:{data.shape}')
-                        data = data.to(device)
-                        optimizer.zero_grad()
-                        output = my_model(data)
-                        loss =  ((data - output)**2).sum() + my_model.encoder.kl 
-                        loss.backward()
-                        optimizer.step()
-                        train_loss += loss.item() * data.size(0) # 이것은 모든 배치의 크기가 일정하지 않을 수 있기 때문에 이렇게 수행함! train_loss는 total loss of batch가 됨
-                    train_loss /= len(train_loader.sampler)            
-                    writer_train.add_scalar('loss(VAE)', train_loss, epoch)
-                    # test session
-                    test_loss = 0
-                    my_model.eval()  # batch norm이나 dropout 등을 train mode 변환
-                    with torch.no_grad():  # autograd engine, 즉 backpropagatin이나 gradient 계산 등을 꺼서 memory usage를 줄이고 속도를 높임
-                        for data in test_loader:
+                    for epoch in range(epochs):
+                        # train session
+                        my_model.train()
+                        train_loss = 0
+                        for batch_idx, (data) in enumerate(tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs}")):
+                            # print(f'datainput shape:{data.shape}')
                             data = data.to(device)
+                            optimizer.zero_grad()
                             output = my_model(data)
                             loss =  ((data - output)**2).sum() + my_model.encoder.kl 
-                            test_loss += loss.item() * data.size(0)
-                        test_loss /= len(test_loader.sampler)
-                        writer_test.add_scalar('loss(VAE)', test_loss, epoch)
+                            loss.backward()
+                            optimizer.step()
+                            train_loss += loss.item() * data.size(0) # 이것은 모든 배치의 크기가 일정하지 않을 수 있기 때문에 이렇게 수행함! train_loss는 total loss of batch가 됨
+                        train_loss /= len(train_loader.sampler)            
+                        writer_train.add_scalar('loss(VAE)', train_loss, epoch)
+                        # test session
+                        test_loss = 0
+                        my_model.eval()  # batch norm이나 dropout 등을 train mode 변환
+                        with torch.no_grad():  # autograd engine, 즉 backpropagatin이나 gradient 계산 등을 꺼서 memory usage를 줄이고 속도를 높임
+                            for data in test_loader:
+                                data = data.to(device)
+                                output = my_model(data)
+                                loss =  ((data - output)**2).sum() + my_model.encoder.kl 
+                                test_loss += loss.item() * data.size(0)
+                            test_loss /= len(test_loader.sampler)
+                            writer_test.add_scalar('loss(VAE)', test_loss, epoch)
 
 
-                    print(f'\nTrain set: Average loss: {train_loss:.4f}, test set: Average loss: {test_loss:.4f}')
-                writer_train.add_hparams(
-                        {"sess": "train", "Type": dataType, "lr": learningRate, "bsize": batch_size, "DS":nameDataset , 'lossFunc':lossFunction, 'emb_dim': embedding_dim}, 
-                        { 
-                            "loss": train_loss,
-                        }, 
-                    ) 
-                writer_test.add_hparams(
-                        {"sess": "test",  "Type": dataType, "lr": learningRate, "bsize": batch_size, "DS":nameDataset, 'lossFunc':lossFunction, 'emb_dim': embedding_dim}, 
-                        { 
-                            "loss": test_loss,
-                        }, 
-                    ) 
-                writer_train.close()
-                writer_test.close()
-                dir_save_torch = join(SaveDir,modelVersion,nameDataset)
-                ensure_dir(dir_save_torch)
-                # 현재 아래 방법은 모델 저장이 안되는 단점이 있음
-                # model_scripted = torch.jit.script(my_model) # Export to TorchScript
-                # model_scripted.save(join(dir_save_torch,f'{dataType}_{numFold}_fold.pt')) # Save
+                        print(f'\nTrain set: Average loss: {train_loss:.4f}, test set: Average loss: {test_loss:.4f}')
+                    writer_train.add_hparams(
+                            {"sess": "train", "Type": dataType, "lr": learningRate, "bsize": batch_size, "DS":nameDataset , 'lossFunc':lossFunction, 'emb_dim': embedding_dim}, 
+                            { 
+                                "loss": train_loss,
+                            }, 
+                        ) 
+                    writer_test.add_hparams(
+                            {"sess": "test",  "Type": dataType, "lr": learningRate, "bsize": batch_size, "DS":nameDataset, 'lossFunc':lossFunction, 'emb_dim': embedding_dim}, 
+                            { 
+                                "loss": test_loss,
+                            }, 
+                        ) 
+                    writer_train.close()
+                    writer_test.close()
+                    dir_save_torch = join(SaveDir,modelVersion,nameDataset)
+                    ensure_dir(dir_save_torch)
+                    # 현재 아래 방법은 모델 저장이 안되는 단점이 있음
+                    # model_scripted = torch.jit.script(my_model) # Export to TorchScript
+                    # model_scripted.save(join(dir_save_torch,f'{dataType}_{numFold}_fold.pt')) # Save
 
-                # 수정된 방법
-                torch.save(my_model.state_dict(), join(dir_save_torch,f'{dataType}_{embedding_dim}_{numFold}_fold')) # 모델 정의가 필요함
-                # 수정된 방법으로 모델 불러올 떄
-                # Model class must be defined somewhere
-                # model.load_state_dict(torch.load(filepath))
-                # model.eval()
+                    # 수정된 방법
+                    torch.save(my_model.state_dict(), join(dir_save_torch,f'{dataType}_{embedding_dim}_{numFold}_fold')) # 모델 정의가 필요함
+                    # 수정된 방법으로 모델 불러올 떄
+                    # Model class must be defined somewhere
+                    # model.load_state_dict(torch.load(filepath))
+                    # model.eval()
 
