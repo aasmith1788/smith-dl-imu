ddef2453cf2f895f9ac7915fdb293d0951e5fd65
orientpine <orientpine@gmail.com>
2022-07-12 13:23:17 -0400

    NOTSENSOR 작업시작

---
+++ training/MODEL/_NOTSENSOR/Pytorch_AE_LSTM/qsub_torch_regression_angle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+# 1셋당 8시간 걸리나..?
+#$ -l h_rt=24:00:00 
+#$ -N RA
+#$ -o ../../result_qsub/regAng/try_20220629_dense_woDG_첫번째데이터_1layer_add
+#$ -j y    
+#$ -m ea
+#$ -l gpus=1
+#$ -l gpu_c=6.0
+#$ -pe omp 8
+# 메모리에 관해서는 아래 주소를 참고
+# https://www.bu.edu/tech/support/research/system-usage/running-jobs/batch-script-examples/#MEMORY
+
+# Keep track of information related to the current job
+echo "=========================================================="
+echo "Start date : $(date)"
+echo "Job name : $JOB_NAME"
+echo "Job ID : $JOB_ID  $SGE_TASK_ID"
+echo "=========================================================="
+
+module load miniconda/4.9.2
+conda activate imu
+python torch_regression_angle.py
---
+++ training/MODEL/_NOTSENSOR/Pytorch_AE_LSTMwithMINI_Demographic/qsub_torch_DGMini_regression_moBWHT.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -l
+
+#$ -l h_rt=12:00:00
+#$ -N MNmo
+#$ -o ../../result_qsub/miniDgRegMo/20220630_Decay_0.0005
+#$ -j y    
+#$ -m ea
+#$ -l gpus=1
+#$ -l gpu_c=6.0
+#$ -pe omp 8
+# 메모리에 관해서는 아래 주소를 참고
+# https://www.bu.edu/tech/support/research/system-usage/running-jobs/batch-script-examples/#MEMORY
+
+# Keep track of information related to the current job
+echo "=========================================================="
+echo "Start date : $(date)"
+echo "Job name : $JOB_NAME"
+echo "Job ID : $JOB_ID  $SGE_TASK_ID"
+echo "=========================================================="
+
+module load miniconda/4.9.2
+conda activate imu
+python torch_DGMini_regression_moBWHT.py
